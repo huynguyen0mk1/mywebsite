679f4c2f2e101b85407eb8c02260bd562cdcbf11
huynguyen0mk1 <50603994+huynguyen0mk1@users.noreply.github.com>
2022-02-27 01:29:51 +0700

    1

--- server.js
+++ server.js
@@ -32,18 +32,14 @@
 // app.listen(port, function () {
 //   routes(app);
 // });
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-const http = require('http');
-
-const hostname = '171.251.17.40';
-const port = 5000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.send('Hello World'))
+  .get('/cool', (req, res) => res.send('Hello World 1'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
